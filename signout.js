ac4481e2adf81efb4dbda75c13534aa709706522
calle361 <calle361rgmail.com>
2024-05-08 10:14:28 +0200

    har fixat sign out

---
+++ signout.js
@@ -0,0 +1,61 @@
+const menu=document.querySelector('#mobile-menu');
+const menuLinks=document.querySelector('.navbar__menu');
+
+menu.addEventListener('click',function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+let signedIn=document.getElementById("signupBtn2");
+let navbarBtn = document.querySelector('.navbar__btn');
+if(sessionStorage.getItem("isLoggedIn")=="true"){
+    console.log(sessionStorage.getItem("isLoggedIn"))
+    //signedIn.innerHTML="signed in";
+    navbarBtn.innerHTML = "<a href='/test5/login.html' class='button'>signed in</a>"
+}
+//page specific-----------------------------------------------------------------------------------
+if(sessionStorage.getItem("isLoggedIn")){
+    var userEmail = sessionStorage.getItem("email");
+    console.log(userEmail);
+
+    document.getElementById("user-email").textContent = userEmail;
+    let signOutButton=document.getElementById("sign-out-button");
+    signOutButton.onclick=function(){
+        signOut();
+        popUpHome();
+    }
+    
+    
+}else{
+    popUp();
+}
+
+
+function popUp(){
+    
+    const confirmed = window.confirm('You need to login before you shuld be able to acess this page');
+    if (confirmed) {
+        location.reload();
+    }
+}
+function signOut(){
+    //sessionStorage.removeItem("email");
+    //sessionStorage.removeItem("isLoggedIn");
+    console.log("Before removing email:", sessionStorage.getItem("email"));
+    console.log("Before removing isLoggedIn:", sessionStorage.getItem("isLoggedIn"));
+  
+    // Remove email and isLoggedIn from sessionStorage
+    sessionStorage.removeItem("email");
+    sessionStorage.removeItem("isLoggedIn");
+  
+    console.log("After removing email:", sessionStorage.getItem("email"));
+    console.log("After removing isLoggedIn:", sessionStorage.getItem("isLoggedIn"));
+
+}
+function popUpHome(){
+    
+    const confirmed = window.confirm('You are signed out. go to home page?');
+    if (confirmed) {
+        window.location.href="/test5/index.html"
+    }
+}
