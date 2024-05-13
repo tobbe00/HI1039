41f2f02cc8da8ac431f6b134a8312d0498b413fa
calle361 <calle361rgmail.com>
2024-05-13 14:05:39 +0200

    nu sparas rounds i slutet av ett spel

--- game.js
+++ game.js
@@ -368,6 +368,35 @@ let totPoints=0;
         })
         .then(data => {
             gameEnded = data
+            if(gameEnded){
+                    // Define the data to send
+                    const data = {
+                        points: totPoints, 
+                        email: sessionStorage.getItem("email") 
+                    };
+                    
+                    // Make a POST request to the endpoint
+                    fetch('http://localhost:8080/rounds/saveRound', {
+                        method: 'POST',
+                        headers: {
+                        'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    })
+                    .then(response => {
+                        if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        console.log('Response:', data);
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                    });
+  
+            }
             
         })
         .catch(error => {
