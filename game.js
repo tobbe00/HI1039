ecbfeb78acfdcc5561cd7a6e4ad8d015c7f4d6f3
tobbe00 <tobiaserlandsson00@hotmail.com>
2024-05-13 12:51:30 +0200

    m

--- game.js
+++ game.js
@@ -14,49 +14,127 @@ if(sessionStorage.getItem("isLoggedIn")=="true"){
     navbarBtn.innerHTML = "<a href='/HI1039/signOut.html' class='button'>sign out</a>"
 }
 let zeroPoint = parseInt(sessionStorage.getItem("zeroPoint"));
+console.log('Original zeroPoint:', zeroPoint);
 
 //page specific!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var backgroundImages = [
+    "gameImages/CPRbackground1.png",
+    "gameImages/CPRbackground2.png",
+    "gameImages/CPRbackground3.png",
+    "gameImages/CPRbackground4.png"
+];
+var currentBackgroundIndex = 0;
+var myGamePiece, myBackGround;
+var testNumbers = [], myTrail = [], greenZoneUpper = [], greenZoneLower = [];
+var currentNum = 0, currentGreenZone = 0, currentGreenZoneLower = 0;
+var gameStarted = false, gameEnded = false, needsReset = false;
 
-var myGamePiece;
-var myBackGround;
-var myTrail = [];
-var greenZone = [];
-var greenZoneLower = [];
-var currentGreenZone = -1;
-var currentGreenZoneLower = -1;
-var newYScale= 100, newY = 25;
-var testNumbers = [300,200,500,700];
-var currentNum = 0;
-var gameStarted = false;
+var toastMessage = "";
+var toastDisplayDuration = 100; // Duration in milliseconds
+var toastTimestamp = 0;
 
+var navbarHeight = document.getElementById('navbarTot').offsetHeight;
+var canvasHeight = window.innerHeight - navbarHeight;
+var scalingFactor = canvasHeight / Math.max(zeroPoint, canvasHeight);
+var dynamicOffset = canvasHeight * 0.01;
+zeroPoint = Math.min(zeroPoint, canvasHeight) - dynamicOffset; 
+var upperGreenZoneY = zeroPoint - (200 * scalingFactor);
+var lowerGreenZoneY = zeroPoint - 50;
+console.log('Scaled zeroPoint:', zeroPoint);
+console.log('Scaling Factor:', scalingFactor);
+console.log('Dynamic Offset:', dynamicOffset);
 
 function startGame() {
-    myGamePiece = new component(0, 16, 16, "gameImages/smiley.gif", 380, zeroPoint, "image");
-    myBackGround = new component(-1447/6000, window.innerWidth,window.innerHeight,"gameImages/graphbackground2.png", 0,0, "image");
-    greenZone.push(new component(-1447/6000, 740, 100, "gameImages/greenSquare.png", 0, 25, "image"));
-    greenZoneLower.push(new component(-1447/6000, 740, 100, "gameImages/greenSquare.png", 0, 350, "image"));
+    
+    myGamePiece = new component(0, 30, 30, "gameImages/Heart.png", 300, zeroPoint, "image");
+    currentBackgroundIndex = 0;
+    myBackGround = new backgroundComponent(-1, window.innerWidth, canvasHeight, backgroundImages[currentBackgroundIndex], 0, 0, "image");
+
+    greenZoneUpper.push(new component(-1, window.innerWidth, 100, "rgba(0, 255, 0, 0.9)", 0, upperGreenZoneY, "color  "));
+    greenZoneLower.push(new component(-1, window.innerWidth, 70, "rgba(0, 255, 0, 0.9)", 0, lowerGreenZoneY, "color"));
+    
     myGameArea.start();
 }
+function resetGame() {
+   
+    myGameArea.stop();
+
+    myTrail = [];
+    greenZoneUpper = [];
+    greenZoneLower = [];
+    testNumbers = [];
+    currentNum = 0;
+    currentGreenZone = 0;
+    currentGreenZoneLower = 0;
+    freq = 0;
+    upperPoints = 0;
+    lowerPoints = 0;
+    totPoints = 0;
+
+    startGame();
+}
+
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
+        this.canvas.height = canvasHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[10]);
         this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
         },
     clear : function() {
+        this.context.save();
+        this.context.setTransform(1, 0, 0, 1, 0, 0);
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.translate(0, -zeroPoint);
+        this.context.restore();
     },
     stop : function() {
         clearInterval(this.interval);
     }
 }
+
+function backgroundComponent(speed, width, height, src, x, y) {
+    this.image = new Image();
+    this.image.src = src;
+    this.width = width;
+    this.height = height;
+    this.speedX = speed;
+    this.x = x;
+    this.y = y;
+    this.nextImage = new Image();
+    this.nextImage.src = backgroundImages[(currentBackgroundIndex + 1) % backgroundImages.length];
+
+    this.update = function () {
+        let ctx = myGameArea.context;
+
+        // Draw the current image
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+
+        // Draw the next image adjacent to the current image
+        ctx.drawImage(this.nextImage, this.x + this.width, this.y, this.width, this.height);
+    }
+
+    this.newPos = function () {
+        this.x += this.speedX;
+
+        // Check if the current background has moved enough to bring in the next image
+        if (this.x <= -this.width) {
+            // Move to the next image
+            this.x = 0;
+            currentBackgroundIndex = (currentBackgroundIndex + 1) % backgroundImages.length;
+
+            // Swap images
+            this.image.src = this.nextImage.src;
+            this.nextImage.src = backgroundImages[(currentBackgroundIndex + 1) % backgroundImages.length];
+        }
+    }
+}
+
+
 
 function component(speed, width, height, color, x, y, type) {
     this.type = type;
@@ -67,14 +145,14 @@ function component(speed, width, height, color, x, y, type) {
     this.width = width;
     this.height = height;
     this.speedX = speed;
-    this.speedY = 0;    
+    this.speedY = 0;
     this.x = x;
-    this.y = y;    
-    this.update = function() {
-        ctx = myGameArea.context;
+    this.y = y;
+    this.update = function () {
+        let ctx = myGameArea.context;
         if (type == "image") {
-            ctx.drawImage(this.image, 
-                this.x, 
+            ctx.drawImage(this.image,
+                this.x,
                 this.y,
                 this.width, this.height);
         } else {
@@ -82,21 +160,13 @@ function component(speed, width, height, color, x, y, type) {
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    this.newPos = function() {
+    this.newPos = function () {
         this.x += this.speedX;
-        this.y +=this.speedY;        
+        this.y += this.speedY;
     }
+
 }
 
-
-
-function moveup() {
-    myGamePiece.speedY -= 1/2;
-}
-
-function movedown() {
-    myGamePiece.speedY += 1/2; 
-}
 function drawText(text, x, y, color = 'black', fontSize = '16px', font = 'Arial') {
     var ctx = myGameArea.context;
     ctx.save();
@@ -115,76 +185,72 @@ function printEvaluationMessageTop(){
 
 }
 function showFrequency(){
-    drawText("BPM: "+freq,660,20,color="black",fontSize = '20px', font = 'Arial');
+    drawText("BPM: "+freq,window.innerWidth-150, 20, color="black",fontSize = '20px', font = 'Arial');
     //drawText(freq,100,80,color="black",fontSize = '20px', font = 'Arial')
 }
 function showTotPoints(){
-    drawText("Total Points: "+totPoints,600,450,color="black",fontSize = '20px', font = 'Arial');
+    drawText("Total Points: "+totPoints, window.innerWidth-350  , 20,color="black",fontSize = '20px', font = 'Arial');
 }
 
 function updateGameArea() {
     var x, y;
-        
-    myBackGround.update();
-    myGameArea.clear();
-
-    if (gameStarted) {
-        myBackGround.update();
-        myBackGround.newPos();      
-        myGameArea.frameNo += 1;
-
-        if (myGameArea.frameNo == 1 || everyinterval(1)) {
-            x = myGamePiece.x;
-            y = myGamePiece.y;
-            myTrail.push(new component(-1447 / 6000, 10, 10, "gameImages/redTrail.png", x, y, "image"));
-        }
-
-
-        if (myGameArea.frameNo == 1 || everyinterval(1500)) {
-            currentGreenZone += 1;
-            newYScale -= 20;
-            newY += 10;
-            greenZone.push(new component(-1447 / 6000, 361.75, newYScale, "gameImages/greenSquare.png", 740, newY, "image"));
-        }
-        if (myGameArea.frameNo == 1 || everyinterval(1500)) {
-            currentGreenZoneLower += 1;
-            greenZone.push(new component(-1447 / 6000, 361.75, 100, "gameImages/greenSquare.png", 740, newY + 325, "image"));
-        }
-
-        /*if(testNumbers[currentNum] / 1.8 > greenZone[currentGreenZone].y && testNumbers[currentNum] / 1.8 < greenZone[currentGreenZone].y + greenZone[currentGreenZone].height){
-            //If sats som kollar om bollen är i den övre grön zonen
-        }
-        if(testNumbers[currentGreenZone] /1.8 > greenZoneLower[currentGreenZoneLower].y){
-            //If sats som kollar om boller är i den undre grön zonen
-        }
     
-        /*if(myGamePiece.y > testNumbers[currentNum]){
-            if(myGamePiece.speedY > 0)
-            {
-                currentNum += 1;
+    myGameArea.clear();
+    
+    
+    if (gameStarted) {
+        needsReset = false;
+        myBackGround.newPos();
+        myBackGround.update();
+        myGameArea.frameNo += 1;
+        
+        if (currentNum >= 0 && currentNum < testNumbers.length) {
+            let targetY = zeroPoint - (testNumbers[currentNum] * scalingFactor);
+            
+            if (myGamePiece.y > targetY) {
+                myGamePiece.speedY = -5;
+                if (myGamePiece.speedY > 0) {
+                    currentNum += 1;
+                }
+            } else if (myGamePiece.y < targetY) {
+                myGamePiece.speedY = 5; 
+                if (myGamePiece.speedY < 0) {
+                    currentNum += 1;
+                }
             }
-            myGamePiece.speedY = -5;
-        }
-        if(myGamePiece.y < testNumbers[currentNum]){
-            if(myGamePiece.speedY < 0)
-            {
-                currentNum += 1;
+            
+            if (currentNum >= testNumbers.length) {
+                currentNum = 0;
             }
-            myGamePiece.speedY = 5;
         }
-        if(currentNum >= testNumbers.length){
-            currentNum = 0;
-        }*/
+        
+        if (myGameArea.frameNo == 1 || everyinterval(1)) {
+            x = myGamePiece.x+10;
+            y = myGamePiece.y+10;
+            myTrail.push(new component(-1, 10, 10, "gameImages/redTrail.png", x, y, "image"));
+        }
 
-        myGamePiece.y = zeroPoint - testNumbers[currentNum];
 
+        if (myGameArea.frameNo == 1 || everyinterval(500)) {
+            currentGreenZone += 1;
+        
+            
+            let lastUpperGreenZone = greenZoneUpper[greenZoneUpper.length - 1];
+            let newUpperGreenZoneX = lastUpperGreenZone.x + lastUpperGreenZone.width;
+            let newUpperGreenZoneHeight = Math.max(lastUpperGreenZone.height - 15, 15);
+            let newUpperGreenZoneY = lastUpperGreenZone.y + (15 / 2);
 
-
-
-        //Alla position updates
-        for (i = 0; i < greenZone.length; i += 1) {
-            greenZone[i].newPos();
-            greenZone[i].update();
+            greenZoneUpper.push(new component(-1, window.innerWidth, newUpperGreenZoneHeight, "rgba(0, 255, 0, 0.9)", newUpperGreenZoneX, newUpperGreenZoneY, "zone"));
+        
+            let lastLowerGreenZone = greenZoneLower[greenZoneLower.length - 1];
+            let newLowerGreenZoneX = lastLowerGreenZone.x + lastLowerGreenZone.width;
+            let newLowerGreenZoneHeight = Math.max(lastLowerGreenZone.height - 10, 10);
+            greenZoneLower.push(new component(-1, window.innerWidth, newLowerGreenZoneHeight, "rgba(0, 255, 0, 0.9)", newLowerGreenZoneX, lowerGreenZoneY + (70 - newLowerGreenZoneHeight), "zone"));
+        }
+        
+        for (i = 0; i < greenZoneUpper.length; i += 1) {
+            greenZoneUpper[i].newPos();
+            greenZoneUpper[i].update();
         }
         for (i = 0; i < greenZoneLower.length; i += 1) {
             greenZoneLower[i].newPos();
@@ -195,31 +261,51 @@ function updateGameArea() {
             myTrail[i].update();
         }
 
-    
-    myGamePiece.newPos();
-    myGamePiece.update();   
-        drawText(`Y: ${Math.round(myGamePiece.y)}`, 10, 20, 'blue', '14px');
-        myGamePiece.newPos();
-        myGamePiece.update();
-    }
-    else{
+
+        if (currentGreenZone >= 0 && currentGreenZone < greenZoneUpper.length) {
+            let upperZone = greenZoneUpper[currentGreenZone];
+             if (myGamePiece.y > upperZone.y && myGamePiece.y < upperZone.y + upperZone.height) {
+                showToast("Upper Zone");
+            }
+        }
+
+        if (currentGreenZoneLower >= 0 && currentGreenZoneLower < greenZoneLower.length) {
+            let lowerZone = greenZoneLower[currentGreenZoneLower];
+            if (myGamePiece.y > lowerZone.y && myGamePiece.y < lowerZone.y + lowerZone.height) {     
+                showToast("Lower Zone");
+            }
+        }
+       
+
+
+    } else{
         myBackGround.update();
-       for (i = 0; i < greenZone.length; i += 1) {
-            greenZone[i].update();
-        }
-        for (i = 0; i < greenZoneLower.length; i += 1) {
-            greenZoneLower[i].update();
-        }
-        for (i = 0; i < myTrail.length; i += 1) {
-            myTrail[i].update();
-        }
+       
+        greenZoneUpper.forEach(zone => zone.update());
+        greenZoneLower.forEach(zone => zone.update());
+        myTrail.forEach(trail => trail.update());
+        
         
     }
+    myGamePiece.newPos();
+    myGamePiece.update();
+    showTotPoints();
+    showFrequency();
+    drawText(`Y: ${zeroPoint - myGamePiece.y}`, 10, 20, 'blue', '14px');
+    drawToast();
+    if(gameEnded && !needsReset){
+        needsReset = true;
+        resetGame();
+    } 
+    
+        
 }
+ 
+    
+        
 
 function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
+    return (myGameArea.frameNo / n) % 1 === 0;
 }
 //about reeding from api!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
@@ -254,7 +340,9 @@ let totPoints=0;
                 }
             }
             oldId=data.id;
-            currentNum = data.id;
+            if (currentNum >= testNumbers.length) {
+                currentNum = testNumbers.length - 1;
+            }
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
@@ -270,19 +358,53 @@ let totPoints=0;
         })
         .then(data => {
             
-             //Process the data
             gameStarted = data
-            console.log(gameStarted)
+            
+            
             
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
         });
-
+        fetch('http://localhost:8080/game/gameEnd')
+        .then(response => {
+           // console.log(response);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            gameEnded = data
+            
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
 
 // Fetch data every 0.1 seconds 1000=1s
 setInterval(fetchData, 100);
+
+function showToast(message) {
+    toastMessage = message;
+    toastTimestamp = Date.now();
+    
+}
+
+function drawToast() {
+    if (toastMessage && Date.now() - toastTimestamp < toastDisplayDuration) {
+        var ctx = myGameArea.context;
+        ctx.save();
+        ctx.font = '20px Arial';
+        
+        ctx.fillStyle = 'red';
+        ctx.fillText(toastMessage, window.innerWidth / 2 - 90, 80);
+        ctx.restore();
+    } else {
+        toastMessage = "";
+    }
+}
 
 
 
